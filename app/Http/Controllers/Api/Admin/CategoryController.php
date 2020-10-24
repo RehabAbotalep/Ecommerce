1307849982786357fca96d4304812c3236a2a838
Rehab Ragab <rehab.ragab@appssquare.com>
2020-10-24 22:33:39 +0200

    First

---
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Category\CategoryCollection;
+use App\Http\Resources\Category\CategoryResource;
+use App\Http\Requests\CategoryRequest;
+use Symfony\Component\HttpFoundation\Response;
+use App\Category;
+
+
+
+
+class CategoryController extends Controller
+{
+    
+	public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return CategoryCollection::collection(Category::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CategoryRequest $request)
+    {
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        
+    }
+}
---
+++ resources/views/Ecommerce/index.blade.php
@@ -0,0 +1,68 @@
+@extends('frontend.master')
+@section('title')
+	Laravel Ecommerce
+@endsection
+@section('content')
+
+    @if(session('success'))
+        <div class="alert alert-success" >
+            {{session('success')}}
+        </div>
+    @endif
+
+ 
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    @foreach($ads as $key=>$ad)
+        <div class="carousel-item{{ $key == 0 ? ' active' : '' }}">
+          <img class="d-block w-100" src="banner/{{$ad->image}}" height="250px">
+        </div>
+    @endforeach
+    
+    
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+        @foreach($products->chunk(4) as $chunkProduct)
+
+            <div class="row">
+                @foreach($chunkProduct as $product)
+                    <div class="col-xs-18 col-sm-6 col-md-3">
+                        <div class="thumbnail" style="margin-left: 10px;">
+                            <a href="{{route('product.show',['id'=>$product->id])}}"><img src="images/{{ $product->image }}" width="150" height="100"></a>
+                            <div class="caption">
+                                <h4>{{ $product->name }}</h4>
+                                <p>{{$product->description}}</p>
+                                <p><strong>Category:</strong> {{$product->category->name}}</p>
+                                <p><strong>Price: </strong> {{ $product->price }}$</p>
+                                <p><strong>Add To List</strong><a href="{{route('whishlist',['id'=>$product->id])}}"><i class="fas fa-heart"></i></a></p>
+                                
+                                    @if($product->review->count()>0)
+                                        <p><strong>Rate:</strong> 
+                                        {{round($product->review->sum('star')/$product->review->count(),2)}}
+                                    
+                                    <strong>Votes:</strong> {{$product->review->count()}}
+                                    </p>
+                                    @else
+                                        <p><strong>Rate:</strong>"No Rating"</p>
+                                    @endif
+                                <p class="btn-holder"><a href="{{route('addToCart',['id'=>$product->id])}}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
+                            </div>
+                        </div>
+                    </div>
+        
+               @endforeach
+        </div>
+            
+        @endforeach
+    @include('partial.footer')
+    
+@endsection
